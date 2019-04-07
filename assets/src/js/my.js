c32d8de021f392bb309055c6ee9dda91da77bbcd
Alexander <frostmoroz83@yandex.ru>
2019-04-07 10:37:38 +0400

    end

--- assets/src/js/my.js
+++ assets/src/js/my.js
@@ -7,53 +7,61 @@
   //slider
   let slideIndex = 1;
 
-  // showSlides(slideIndex);
-  // function showSlides(n) {
-  //   if (n > slides.length) {
-  //     slideIndex  =1;
-  //   }
-  //   if (n<1) {
-  //     slideIndex = slides.length;
-  //   }
+  showSlides(slideIndex);
+  function showSlides(n) {
+    if (n > slides.length) {
+      slideIndex  =1;
+    }
+    if (n<1) {
+      slideIndex = slides.length;
+    }
 
-  //   slides.forEach((item) => item.style.display = 'none');
-  //   // for (let i = 0; i < slides.length; i++) {
-  //   //   slides[i].style.display = 'none';
-  //   // }
-  //   // dots.forEach((item) => item.classList.remove('dot-active'));
-  //   slides[slideIndex - 1].style.display = 'block';
-  //   // dots[slideIndex - 1].classList.add('dot-active');
-  // }
+    slides.forEach((item) => item.style.display = 'none');
+    // for (let i = 0; i < slides.length; i++) {
+    //   slides[i].style.display = 'none';
+    // }
+    // dots.forEach((item) => item.classList.remove('dot-active'));
+    slides[slideIndex - 1].style.display = 'block';
+    // dots[slideIndex - 1].classList.add('dot-active');
+  }
 
-  // function plusSlides(n) {
-  //   showSlides(slideIndex += n);
-  // }
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
 
-  // prev.addEventListener('click', function(){
-  //   plusSlides(-1);
-  // });
+  prev.addEventListener('click', function(){
+    plusSlides(-1);
+  });
 
-  // next.addEventListener('click', function(){
-  //   plusSlides(1);
-  // });
+  next.addEventListener('click', function(){
+    plusSlides(1);
+  });
 
   //end slider
 
   //product
 
   let productItem = document.querySelectorAll('.product_item');
-  let productFooter = document.querySelector('.product_item_footer');
+  let productFooter = document.querySelectorAll('.product_item_footer');
 
-  console.log(productItem);
-  console.log(productFooter);
+  // console.log(productItem);
+  // console.log(productFooter);
 
-  productItem.onmouseover = function () {
-    productFooter.style.display = 'block';
-    
-  };
-  productItem.onmouseout = function () {
-    document.getElementById('a').style.backgroundColor = '';
-  };
+  for (let i = 0; i < productItem.length; i++) {
+        // slides[i].style.display = 'none';
+        // console.log(productItem[i]);
+        productItem[i].onmouseover = function () {
+        productFooter[i].style.display = 'block';
+          
+        };
+        productItem[i].onmouseout = function () {
+          productFooter[i].style.display = 'none';
+        };
+
+      }
+
+
+  
 
 
 }());
